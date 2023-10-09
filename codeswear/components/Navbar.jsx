16c7d822457dfd965d52fbe87ae845d01732edef
rishisAryal <risharyal5@gmail.com>
2023-10-09 13:36:53 +0545

    changes

--- codeswear/components/Navbar.jsx
+++ codeswear/components/Navbar.jsx
@@ -133,9 +133,14 @@ const Navbar = () => {
             </div>
           </li>
         </ul>
-        <button className="flex w-fit  text-center  mt-10 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
-          <BsFillBagFill className="m-1" /> Checkout
-        </button>
+        <div className="flex  justify-center gap-1">
+          <button className="flex w-fit  text-center  mt-10 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded">
+            <BsFillBagFill className="m-1" /> Checkout
+          </button>
+          <button className="flex w-fit  text-center  mt-10 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded">
+            Clear Cart
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ codeswear/pages/api/pincode.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json([23400, 721302, 11003, 560017]);
+}
